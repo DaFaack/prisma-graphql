fa1ab1c9c9a61b5fadd12a24047cf654b76c8b26
DaFaack <aaron.wtal@gmail.com>
2021-12-01 18:37:23 +0700

    Wtf

--- prisma/migrations/20211125043316_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-
-    PRIMARY KEY ("id")
-);
