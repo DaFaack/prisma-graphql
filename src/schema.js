2c47ebbcb2e5e405674e16e61051048e14e90485
DaFaack <aaron.wtal@gmail.com>
2021-12-02 15:27:27 +0700

    published true

--- src/schema.js
+++ src/schema.js
@@ -60,7 +60,7 @@ const resolvers = {
         data: {
           title: args.title,
           content: args.content,
-          published: false,
+          published: true,
           author: args.authorEmail && {
             connect: { email: args.authorEmail },
           },
