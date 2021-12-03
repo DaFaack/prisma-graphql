384a2425c1b6c5e7e18c47b6f63999af4d0bea22
DaFaack <aaron.wtal@gmail.com>
2021-12-03 12:15:10 +0700

    new migration

--- prisma/migrations/20211203044906_meme/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Meme" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "button_normal" TEXT NOT NULL,
-    "button_pressed" TEXT NOT NULL,
-
-    CONSTRAINT "Meme_pkey" PRIMARY KEY ("id")
-);
